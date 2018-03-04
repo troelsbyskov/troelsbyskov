d53c3b7a568b6686baece18be3015150a4cb90bd
troelsbyskov <troels_post@outlook.com>
2018-03-04 19:16:51 +0000

    save

--- feedback.php
+++ feedback.php
@@ -10,13 +10,13 @@
             $name = $_POST['feedbackname'];
             $email = $_POST['feedbackemail'];
             $message = $_POST['feedbackcontent'];
-            $from = 'From: Hjemmeside-feedback';
-            $to = 'troels.byskov@gmail.com';
-            $subject = 'Feedback fra hjemmeside';
+            $from = "Hjemmeside-feedback";
+            $to = "troels.byskov@gmail.com";
+            $subject = "Feedback fra hjemmeside";
 
             $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-            if ($_POST['submit']) {
+            if ($_POST['feedbacksubmit']) {
                 if (mail ($to, $subject, $body, $from)) {
                 echo '<p>Din feedback er sendt. Mange tak!</p>';
             } else {
