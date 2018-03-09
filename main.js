5cca794c9c8b3c6d0bff779bb3fe1f0026de7add
troelsbyskov <troels_post@outlook.com>
2018-03-09 11:28:30 +0000

    save

--- main.js
+++ main.js
@@ -16,3 +16,42 @@ window.onclick = function(event) {
     }
   }
 }
+/* Slut på Dropdown menu */
+
+/* Popups */
+function myPopup() {
+    document.getElementById('light').style.display='block';
+    document.getElementById('fade').style.display='block';
+    document.getElementById('kryds').style.display='block';
+}
+
+function myPopup2() {
+    document.getElementById('light2').style.display='block';
+    document.getElementById('fade').style.display='block';
+    document.getElementById('kryds').style.display='block';
+}
+
+function closePopup() {
+    document.getElementById('light').style.display='none';
+    document.getElementById('light2').style.display='none';
+    document.getElementById('fade').style.display='none';
+    document.getElementById('kryds').style.display='none';
+}
+/* Slut på Popups */
+
+/* Back to top */
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("back-to-top").style.display = "block";
+    } else {
+        document.getElementById("back-to-top").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+/*Slut på Back to top*/
