193f7da8258b47f358ca66a3c273cfb043c98f7b
troelsbyskov <troels_post@outlook.com>
2018-03-09 16:25:16 +0000

    save

--- main.js
+++ main.js
@@ -43,15 +43,46 @@ function closePopup() {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    if (document.body.scrollTop > 515 || document.documentElement.scrollTop > 515) {
         document.getElementById("back-to-top").style.display = "block";
     } else {
         document.getElementById("back-to-top").style.display = "none";
     }
 }
-
-function topFunction() {
-    document.body.scrollTop = 0; // For Chrome, Safari and Opera
-    document.documentElement.scrollTop = 0; // For IE and Firefox
-}
 /*Slut på Back to top*/
+
+/* Expand/collapse text */
+function toggleMe(a){
+    var e=document.getElementById(a);
+    var i = document.getElementById(a+'_image');
+        if(!e)return true;
+        if(e.style.display=="none"){
+            e.style.display="block"
+            i.style.backgroundPosition="0 -100%";
+        } else {
+            e.style.display="none"
+            i.style.backgroundPosition="0 0";
+        }
+        return true;
+}
+/* Slut på Expand/collapse text */
+
+/* Scrollmenu-dropdown */
+function smDropdownMenu() {
+    document.getElementById("smDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.smdropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("smdropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+/* Slut på Scrollmenu-dropdown */
